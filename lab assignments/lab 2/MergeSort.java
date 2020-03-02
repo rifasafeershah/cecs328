4acbf3b9f2ad2e95c4dfc4fa4844f044eb6c67c6
ManuelCastroMirafuentes001 <manuel.castromirafuentes001@gmail.com>
2020-03-01 21:14:41 -0800

    Update MergeSort.java

--- lab assignments/lab 2/MergeSort.java
+++ lab assignments/lab 2/MergeSort.java
@@ -6,10 +6,13 @@
 
 public class MergeSort
 {
-	public void merge(int array[], int l, int m, int r)
+	int count;
+	
+	public void Merge(int array[], int l, int m, int r)
 	{
 		int n1 = m - l + 1;
 		int n2 = r - m;
+		
 		int left[] = new int [n1];
 		int right[] = new int [n2];
 		
@@ -25,12 +28,12 @@ public class MergeSort
 		
 		for(int k = l; k < r; k++)
 		{
-			count++;
-			for(int i = 0; i < n1, i++)
+			for(int i = 0; i < n1; i++)
 			{
-				for(int j = 0, j < n2, j++)
+				for(int j = 0; j < n2; j++)
 				{
-					if(left[i] <= right[j];
+					count++;
+					if(left[i] <= right[j]);
 					{
 						array[k] = left[i];
 					}
@@ -40,7 +43,11 @@ public class MergeSort
 					}
 				}
 			}
-			for(int i = 0; i < n1, i++)
+		}
+		
+		for(int k = l; k < r; k++)
+		{
+			for(int i = 0; i < n1; i++)
 			{
 				array[k] = left[i];
 			}
@@ -54,14 +61,14 @@ public class MergeSort
   	//p is left
   	//q is middle
   	//r is right
-	public void Sort(int array[], int mLeft, int mRight)
+	public void mergeSort(int array[], int mLeft, int mRight)
 	{
 		if(mLeft < mRight)
 		{
 			int mMiddle = (mLeft + mRight) / 2;
-			Sort(array, mLeft, mMiddle);
-			Sort(array, mMiddle + 1, mRight);
-			Sort(array, mLeft, mMiddle, mRight);	
+			mergeSort(array, mLeft, mMiddle);
+			mergeSort(array, mMiddle + 1, mRight);
+			Merge(array, mLeft, mMiddle, mRight);	
 		}
 	}
 	
@@ -87,12 +94,12 @@ public class MergeSort
 		printArray(array);
 		
 		MergeSort ms = new MergeSort();
-		ms.sort(array, 0, array.length - 1);
+		m.Count = 0;
+		ms.Sort(array, 0, array.length - 1);
 		
 		System.out.println("\nSorted Array");
 		printArray(array);
 		
-		mCount = 0;
 		System.out.println("\nNumber of Comparisions:" + m.Count);
 	} //Ends main
 } //Ends MergeSort
