45034dedd53056964100a3b7de024b23f806a74e
Rifa Safeer Shah <rifashah28@gmail.com>
2020-03-01 18:37:50 -0800

    Update MergeSort.java

--- lab assignments/lab 2/MergeSort.java
+++ lab assignments/lab 2/MergeSort.java
@@ -1,15 +1,22 @@
 class MergeSort {
-  void merge(int array[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
+  void merge(int array[], int left, int middle, int right) {
+    int p1 = middle - left + 1;
+    int p2 = right - middle;
     
-    int Left[] = new int [n1];
-    int Right[] = new int [n2];
+    int Left[] = new int [p1];
+    int Right[] = new int [p2];
     
-    for (int i = 0; i < n1; ++i){
-      Left[i]  = array[l + i];
+    for (int i = 0; i < p1; ++i){
+      Left[i]  = array[left + i];
     }
     
-    for (int j = 0; i < n2; ++j){
-      Right[j] = array[m + 1 + j];
+    for (int j = 0; i < p2; ++j){
+      Right[j] = array[middle + 1 + j];
     }
+    
+    int i = 0;
+    int j = 0;
+    
+    int k = left;
+    while(i < p1 && j < p2) {
+      if (Left[i]
