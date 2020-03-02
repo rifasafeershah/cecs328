7e3e278e5abae32be04d4522395a93f54071ed56
Rifa Safeer Shah <rifashah28@gmail.com>
2020-03-01 19:11:58 -0800

    Update MergeSort.java

--- lab assignments/lab 2/MergeSort.java
+++ lab assignments/lab 2/MergeSort.java
@@ -12,11 +12,11 @@ class MergeSort {
     int Left[] = new int [p1];
     int Right[] = new int [p2];
     
-    for (int i = 0; i < p1; ++i){
+    for (int i = 0; i < p1; ++i) {
       Left[i]  = array[left + i];
     } //Ends for loop
     
-    for (int j = 0; i < p2; ++j){
+    for (int j = 0; i < p2; ++j) {
       Right[j] = array[middle + 1 + j];
     } //Ends for loop
     
@@ -27,4 +27,25 @@ class MergeSort {
     //while(i < p1 && j < p2) {
       //if (Left[i]
   } //Ends merge
+  
+  void sort() {
+    
+  }
+  
+  public static void main(String args[]) {
+    int[] array = new int [20];
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter elements: ");
+    for (int i = 0; i < a.length; i++) {
+      System.out.print(a[i]);
+    } //Ends for loop
+    System.out.println("Given Array");
+    printArray(array);
+    
+    MergeSort ms = new MergeSort();
+    ms.sort(array, 0, array.length-1);
+    
+    System.out.println("\nSorted Array");
+    printArray(array);
+  } //Ends main
 } //Ends MergeSort
