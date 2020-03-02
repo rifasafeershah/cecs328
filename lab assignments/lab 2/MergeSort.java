bf3412f8ab0e48a6def859938e19fd7aee23d4e1
Rifa Safeer Shah <rifashah28@gmail.com>
2020-03-01 18:28:42 -0800

    Update MergeSort.java

--- lab assignments/lab 2/MergeSort.java
+++ lab assignments/lab 2/MergeSort.java
@@ -1,1 +1,15 @@
-
+class MergeSort {
+  void merge(int array[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    
+    int Left[] = new int [n1];
+    int Right[] = new int [n2];
+    
+    for (int i = 0; i < n1; ++i){
+      Left[i]  = array[l + i];
+    }
+    
+    for (int j = 0; i < n2; ++j){
+      Right[j] = array[m + 1 + j];
+    }
